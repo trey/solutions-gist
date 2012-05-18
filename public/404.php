7b20ff12f730259743f8908c539950e041f2b690
Trey Piepmeier <trey@treypiepmeier.com>
2012-05-17 22:39:19 -0500

    Solutions Log as an index of Gists

---
+++ public/404.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Page not found. :( &mdash; Trey Piepmeier's Solutions Log</title>
+  <meta name="description" content="Solutions to little development problems. Welcome, Google visitor.">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="/css/html5bp.css">
+  <link rel="stylesheet" href="/css/solutions.css">
+</head>
+<body>
+  
+  <h1>Sorry. Couldn't find that. :(</h1>
+
+  <p>I recently cleaned out a bunch of old Solutions Log posts that were as old as the hills.</p>
+  <p>See <a href="/">what's fresh</a>.</p>
+
+  <?php require_once('../_tracking.php'); ?>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+date_default_timezone_set('America/Chicago');
+require_once('../simplepie.inc');
+$feed = new SimplePie();
+$feed->set_feed_url('http://feeds.pinboard.in/rss/secret%3Ad2eb5ef4f7bb90a90d31/u%3Atrey/t%3Asolutions/');
+$feed->init();
+$feed->handle_content_type();
+?><!doctype html>
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Trey Piepmeier's Solutions Log</title>
+  <meta name="description" content="Solutions to little development problems. Welcome, Google visitor.">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="/css/html5bp.css">
+  <link rel="stylesheet" href="/css/solutions.css">
+</head>
+<body>
+  
+  <h1><a href="http://treypiepmeier.com">Trey Piepmeier</a>'s Solutions Log</h1>
+
+  <?php foreach ($feed->get_items() as $item): ?>
+    <?php $title = htmlentities(preg_replace('/— Gist/', '', $item->get_title())); ?>
+    
+    <ul>
+      <li><a href="<?php echo $item->get_permalink(); ?>"><?php echo $title; ?></a> <span class="date"><?php echo $item->get_date('F j, Y'); ?></span></li>
+    </ul>
+
+  <?php endforeach; ?>
+
+  <?php require_once('../_tracking.php'); ?>
+</body>
+</html>
